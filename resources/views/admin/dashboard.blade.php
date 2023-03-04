288a492bdb42aacb9c5b4a1bd1f0e4cd23da1ae0
wildannouval <senangkucing@gmail.com>
2023-03-04 17:41:48 +0700

    Final commit

---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,4 @@
+@extends('layouts.admin.app', ['title' => 'Dashboard'])
+
+@section('content')
+@endsection 
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::group(['prefix' => 'admin', 'as' => 'admin.', 'middleware' => ['auth']], function(){
+    Route::get('/dashboard', \App\Http\Controllers\admin\DashboardController::class)->name('dashboard');
+});
